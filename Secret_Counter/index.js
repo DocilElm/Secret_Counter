aa8e71d510b9cbf403d7a082eea9731cca2cab0d
DocilElm <65479309+DocilElm@users.noreply.github.com>
2022-09-30 16:17:34 -0700

    Add files via upload

--- Secret_Counter/index.js
+++ Secret_Counter/index.js
@@ -1,30 +1,26 @@
 /// <reference types="../CTAutocomplete" />
 import request from 'request/index';
 
-var apikey = "c2709309-ded7-4244-a9a5-748f1902c20f";
-
 register('command', Show_Secrets).setCommandName('sc');
 
 const get_uuid = (username) => request({url : `https://playerdb.co/api/player/minecraft/${username}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
-const get_secrets = (username, cute_name) => request({url : `https://sky.shiiyu.moe/api/v2/dungeons/${username}/${cute_name}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
-const get_profile = (apikey, user_uuid) => request({url : `https://api.hypixel.net/skyblock/profiles?key=${apikey}&uuid=${user_uuid}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
+const get_secrets = (username) => request({url : `https://sky.shiiyu.moe/api/v2/dungeons/${username}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
 const get_rank = (username) => request({url : `https://api.slothpixel.me/api/players/${username}`,headers: { 'User-Agent': ' Mozilla/5.0' }}).then(response => JSON.parse(response)).catch(error =>{ print(error);});
 
-const get_profile_id = (user_uuid, profiles=null, apikey=null) => {
-    const getRecent = (profiles) => !profiles.profiles || !profiles.profiles.length ? null : profiles.profiles.find(a => a.selected) ?? profiles[0]
-    if (profiles) return getRecent(profiles)
-    return get_profile(apikey, user_uuid).then(profiles => getRecent(profiles)).catch(e => print(`${e}`))
-}
 function Show_Secrets(username) {
     if(!username){username = Player.getName()}
   get_uuid(username).then(rank_data => {
     let name_ = rank_data.data.player.username;
-    let user_uuid = rank_data.data.player.raw_id;
-    get_profile_id(user_uuid, null, apikey).then(res => {
-    let profile_id = res.profile_id;
-    let cute_name = res.cute_name;
-    get_secrets(username, cute_name).then(secrets_data => {
-        var secrets = secrets_data.dungeons.secrets_found;
+    get_secrets(username).then(data => {
+        let profile_
+		for (profile in data["profiles"]) {
+			if (profile_ == undefined || profile_["dungeons"]["secrets_found"] < data["profiles"][profile]["dungeons"]["secrets_found"]) {
+				profile_ = data["profiles"][profile]
+			}
+		}
+        let secrets = profile_.dungeons.secrets_found;
+        secrets = Math.trunc(secrets);
+        secrets = secrets.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
         get_rank(username).then(rank_data => {
             var rank = rank_data.rank_formatted;
             name_ = `${rank} ${name_}`
@@ -36,11 +32,7 @@ function Show_Secrets(username) {
     }).catch(function(error) {
         print(error)
         ChatLib.chat("§2[Secrets Counter] §4Error Getting Player's Secrets!")
-     });
-    }).catch(function(error) {
-        print(error)
-        ChatLib.chat("§2[Secrets Counter] §4Error Getting Player's Profile Data!")
-     });
+     });    
 }).catch(function(error) {
     print(error)
     ChatLib.chat("§2[Secrets Counter] §4Error Getting Player's UUID!")
